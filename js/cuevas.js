619feb1b94e32b9e4f857a58a833349a27e8829f
oscarsaizgu <oscarsaizgu@gmail.com>
2026-08-07 14:32:29 +0200

    streetview

--- js/cuevas.js
+++ js/cuevas.js
@@ -29,8 +29,6 @@ var elevacion = L.control.elevation({
 elevacion.addTo(mapa);
 elevacion.load('data/cuevas.gpx');
 
-var maxBoundsRuta;
-
 new L.GPX('data/cuevas.gpx', {
     async: true,
     polyline_options: {
@@ -47,18 +45,17 @@ new L.GPX('data/cuevas.gpx', {
 }).on('loaded', function(e) {
     var bounds = e.target.getBounds();
     mapa.fitBounds(bounds);
-    maxBoundsRuta = bounds.pad(0,5);
-    mapa.setMaxBounds(maxBoundsRuta);
     mapa.options.minZoom = mapa.getZoom();
 }).addTo(mapa);
 
 var puntosInteres = [
-    { coords: [43.244719, -3.454010], nombre: "Mirador de Covalanas", foto: "fotos/mirador.jpg" },
-    { coords: [43.245467, -3.452144], nombre: "Cueva de Covalanas", foto: "fotos/covalanas.jpg" },
-    { coords: [43.245171, -3.452452], nombre: "Cueva del Mirón", foto: "fotos/miron.jpg" },
-    { coords: [43.244278, -3.450562], nombre: "Cueva de la Luz", foto: "fotos/luz.jpg" },
-    { coords: [43.248049, -3.456690], nombre: "Cueva el Haza", foto: "fotos/haza.jpg" },
-    { coords: [43.255676, -3.458022], nombre: "Cueva de Cullalvera", foto: "fotos/cullalvera.jpg" }
+    { coords: [43.244719, -3.454010], nombre: "Mirador de Covalanas", foto: "fotos/mirador.jpg", streetview: null },
+    { coords: [43.245467, -3.452144], nombre: "Cueva de Covalanas", foto: "fotos/covalanas.jpg", streetview: null },
+    { coords: [43.245171, -3.452452], nombre: "Cueva del Mirón", foto: "fotos/miron.jpg", streetview: null },
+    { coords: [43.244278, -3.450562], nombre: "Cueva de la Luz", foto: "fotos/luz.jpg", streetview: null },
+    { coords: [43.248049, -3.456690], nombre: "Cueva el Haza", foto: "fotos/haza.jpg", streetview: null },
+    { coords: [43.255676, -3.458022], nombre: "Cueva de Cullalvera", foto: "fotos/cullalvera.jpg", streetview: null },
+    { coords: [43.243676959842375, -3.4515943412797148], nombre: "Mirador Pared del Eco", foto: null, streetview: "https://www.google.com/maps/embed?pb=!4v1786105827670!6m8!1m7!1sCAoSFkNJSE0wb2dLRUlDQWdJRHlqZUhfQ2c.!2m2!1d43.24362767947232!2d-3.451567598630545!3f102.16397789636218!4f0!5f0.7820865974627469" }
 ];
 
 var iconoMarker = L.divIcon({
@@ -72,25 +69,31 @@ puntosInteres.forEach(function(punto) {
     var marker = L.marker(punto.coords, { icon: iconoMarker }).addTo(mapa);
 
     marker.on('mouseover', function() {
-        if (punto.nombre === 'Cueva de Cullalvera') {
-            mapa.setMaxBounds(null);
+        var contenido = '<b>' + punto.nombre + '</b>';
+        if (punto.foto) {
+            contenido += '<br><img src="' + punto.foto + '" style="width:150px; margin-top:5px; border-radius:4px;">';
         }
-        this.bindPopup(
-            '<b>' + punto.nombre + '</b><br>' +
-            '<img src="' + punto.foto + '" style="width:150px; margin-top:5px; border-radius:4px;">',
-            { closeButton: false, maxWidth: 200 }
-        ).openPopup();
+        this.bindPopup(contenido, { closeButton: false, maxWidth: 200, autoPan: false }).openPopup();
     });
 
     marker.on('mouseout', function() {
         this.closePopup();
-        if (punto.nombre === 'Cueva de Cullalvera') {
-            mapa.setMaxBounds(maxBoundsRuta);
-        }
     });
 
     marker.on('click', function() {
-        document.getElementById('lightbox-img').src = punto.foto;
+        var iframe = document.getElementById('lightbox-iframe');
+        var img = document.getElementById('lightbox-img');
+
+        if (punto.streetview) {
+            iframe.src = punto.streetview;
+            iframe.style.display = 'block';
+            img.style.display = 'none';
+        } else if (punto.foto) {
+            img.src = punto.foto;
+            img.style.display = 'block';
+            iframe.style.display = 'none';
+            iframe.src = '';
+        }
         document.getElementById('lightbox-titulo').textContent = punto.nombre;
         document.getElementById('lightbox').style.display = 'flex';
     });
@@ -98,9 +101,10 @@ puntosInteres.forEach(function(punto) {
 
 var lightbox = document.createElement('div');
 lightbox.id = 'lightbox';
-lightbox.innerHTML = '<div id="lightbox-contenido"><span id="lightbox-cerrar">✕</span><img id="lightbox-img"><p id="lightbox-titulo"></p></div>';
+lightbox.innerHTML = '<div id="lightbox-contenido"><span id="lightbox-cerrar">✕</span><img id="lightbox-img"><iframe id="lightbox-iframe" style="display:none;width:100%;height:300px;border:0;" allowfullscreen="" loading="lazy"></iframe><p id="lightbox-titulo"></p></div>';
 document.body.appendChild(lightbox);
 
 document.getElementById('lightbox-cerrar').addEventListener('click', function() {
     lightbox.style.display = 'none';
+    document.getElementById('lightbox-iframe').src = '';
 });
